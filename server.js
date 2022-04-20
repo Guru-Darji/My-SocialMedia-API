e6d04081309eee15cd100d95ee870803a83db2cc
Guru Darji <gurudarji@ymail.com>
2022-04-20 13:39:00 -0400

    setting up server

---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/My-SocialMedia-API', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(` Connected on localhost:${PORT}`));
